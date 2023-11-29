b5add5d35c6198535acccfca724f7d792763f945
Kim5ungkwang <ksg6736@gmail.com>
2023-11-29 16:02:15 +0900

    리펙토링

---
+++ src/main/java/org/example/ComboLetter.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.swing.JLabel;
+
+public class ComboLetter implements Runnable{
+
+    private static JLabel singleLabel = new JLabel("Single");
+    private static JLabel doubleLabel = new JLabel("Double");
+    private static JLabel tripleLabel = new JLabel("Triple");
+    private static JLabel tetrisLabel = new JLabel("tetris!");
+
+
+    @Override
+    public void run() {
+
+    }
+}
--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -1,14 +1,21 @@
 package org.example;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import org.example.data.Tetrominoes;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import org.example.entity.BlockImg;
 
 public abstract class Square extends JPanel {
     public abstract int squareWidth();
     public abstract int squareHeight();
+    public void drawImgSquare(Graphics g, int x, int y, Tetrominoes shape) throws IOException {
+        BufferedImage bufferedImage = BlockImg.getImage(shape);
+        g.drawImage(bufferedImage, x, y, squareWidth(), squareHeight(), null);
+    }
     public void drawSquare(Graphics g, int x, int y, Tetrominoes shape) {
         Color[] colors = { new Color(0, 0, 0), new Color(204, 102, 102), new Color(102, 204, 102),
                 new Color(102, 102, 204), new Color(204, 204, 102), new Color(204, 102, 204), new Color(102, 204, 204),
