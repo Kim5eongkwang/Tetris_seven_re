9f4d38abc76020e4de691527c13d49b4cadd96a6
Kim5ungkwang <ksg6736@gmail.com>
2023-12-03 02:38:39 +0900

    시퀸스 다이어그램 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,9 @@ import javax.swing.JFrame;
 
 public class  Main {
     public static void main(String[] args) throws IOException {
-        LoginScreen login= new LoginScreen();
-        login.setVisible(true);
-        //MainPage page = new MainPage();
-        //page.setVisible(true);
+        //LoginScreen login= new LoginScreen();
+        //login.setVisible(true);
+        MainPage page = new MainPage();
+        page.setVisible(true);
     }
 }
